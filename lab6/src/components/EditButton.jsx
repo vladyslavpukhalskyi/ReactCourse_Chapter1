fd59f96d4f4be74c57cb445c523614dc8838ca16
lonellylennon <vladislavackerman@gmail.com>
2024-10-04 15:03:38 +0300

    validate

--- lab6/src/components/EditButton.jsx
+++ lab6/src/components/EditButton.jsx
@@ -3,16 +3,20 @@ import React, { useState } from 'react';
 const EditButton = ({ toDo, onUpdate }) => {
   const [isEditing, setIsEditing] = useState(false);
   const [editValue, setEditValue] = useState(toDo.title);
+  const [error, setError] = useState(false);
 
   const handleEditClick = () => {
     setIsEditing(true);
   };
 
   const handleSaveClick = () => {
-    if (editValue.trim() !== "") {
+    if (editValue.trim() === "") {
+      setError(true);
+    } else {
       onUpdate(toDo.id, editValue);
+      setIsEditing(false);
+      setError(false);
     }
-    setIsEditing(false);
   };
 
   return (
@@ -23,7 +27,9 @@ const EditButton = ({ toDo, onUpdate }) => {
             type="text"
             value={editValue}
             onChange={(e) => setEditValue(e.target.value)}
+            style={{ borderColor: error ? 'red' : 'initial' }}
           />
+          {error && <div style={{ color: 'red' }}>Empty Field</div>}
           <button onClick={handleSaveClick}>Save</button>
         </>
       ) : (
